9ecd41e7c172b16324bf64640e2f857159cc30b1
xxiyun <wesleylwu@gmail.com>
2026-01-26 23:32:35 -0800

    self closing tag and delete example component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import OurMissionComponent from "@/components/home/ourMission";
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <OurMissionComponent></OurMissionComponent>
+      <OurMissionComponent />
     </div>
   );
 };
