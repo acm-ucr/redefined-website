090c46538a6fc3db94053a9ed816b4c5e5d5da81
APerson09 <ethanhuang63@gmail.com>
2026-01-26 23:29:22 -0800

    Made 404 Page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,27 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
-import Line from "@/public/Vector 2.webp"
+import Line from "@/public/Vector 2.webp";
 
 const NotFound = () => {
-  return <div className = "flex flex-col items-center justify-center h-screen bg-redefined-olive-50">
-      <div className = "flex flex-col justify-center items-center w-150 h-75 bg-redefined-cream rounded-2xl">
-        <div className = "font-bold italic text-redefined-rust text-7xl font-redefined-zilla">
+  return (
+    <div className="bg-redefined-olive-50 flex h-screen flex-col items-center justify-center">
+      <div className="bg-redefined-cream flex h-75 w-150 flex-col items-center justify-center rounded-2xl">
+        <div className="text-redefined-rust font-redefined-zilla text-7xl font-bold italic">
           404
         </div>
-        <div className = "text-black text-2xl font-redefined-albert">
+        <div className="font-redefined-albert text-2xl text-black">
           page not found
         </div>
-        <div className = "text-black text-2xl pt-12 font-redefined-albert ">
+        <div className="font-redefined-albert pt-12 text-2xl text-black">
           return to
-          <Link href="http://localhost:3000/" className = "ml-1 text-redefined-rust text-2xl underline underline-offset-1 italic font-redefined-albert">
+          <Link
+            href="http://localhost:3000/"
+            className="text-redefined-rust font-redefined-albert ml-1 text-2xl italic underline underline-offset-1"
+          >
             home page
           </Link>
         </div>
-        <div className = "absolute bg-redefined-olive w-150 h-75 -z-1 -translate-x-4 translate-y-4 rounded-2xl">
-        </div>
+        <div className="bg-redefined-olive absolute -z-1 h-75 w-150 -translate-x-4 translate-y-4 rounded-2xl"></div>
       </div>
-      <Image src = {Line} alt = "Line"/>
-  </div>;
+      <Image src={Line} alt="Line" />
+    </div>
+  );
 };
 
 export default NotFound;
