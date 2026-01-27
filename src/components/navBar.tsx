8565068cdb7bfbddadfa3cb0e99280ef8e4621b4
Kaesha Inocencio <freeforallyeet123@gmail.com>
2026-01-26 22:14:40 -0800

    edited navbar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,27 +1,42 @@
 "use client";
 import { navigationItems } from "@/data/navigation";
+import Logo from "@/public/redefineD_logo.webp";
 import Link from "next/link";
 import Image from "next/image";
-import Logo from "@/public/redefineD_logo.webp";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="bg-redefined-olive flex items-center justify-between px-8 py-4">
-      <div className="flex h-10 w-50 items-center">
+      {/* Logo */}
+      <Link
+        href="/home"
+        className="flex h-10 w-50 items-center transition-transform duration-150 ease-out hover:scale-105"
+      >
         <Image src={Logo} alt="Logo" />
-      </div>
+      </Link>
 
+      {/* Navigation Links */}
       <div className="flex gap-10">
-        {navigationItems.map((item) => (
-          <Link
-            key={item.link}
-            href={item.link}
-            target="_blank"
-            className="text-redefined-cream font-redefined-alfa hover:text-redefined-taupe text-xl"
-          >
-            {item.name}
-          </Link>
-        ))}
+        {navigationItems.map((item) => {
+          const isActive = pathname === item.link;
+
+          return (
+            <Link
+              key={item.link}
+              href={item.link}
+              className={`font-redefined-alfa text-xl transition-transform duration-150 ease-out hover:scale-105 ${
+                isActive
+                  ? "text-redefined-rust"
+                  : "text-redefined-cream hover:text-redefined-taupe"
+              }`}
+            >
+              {item.name}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
