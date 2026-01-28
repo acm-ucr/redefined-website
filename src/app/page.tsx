3c867550de2d4ecf3ebc4bd52aac82645f4e5b26
EveningSt3r <daawesomeboi@gmail.com>
2026-01-28 01:14:41 -0800

    Build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,4 @@
-import BoardCard from "@/components/board/boardCard";
-import { boardMembers } from "@/data/boardInfo";
-
-const BoardEntry = () => {
-  return (
-    <div className="bg-redefined-olive-50 min-h-screen px-4 py-10">
-      <p className="font-redefined-zilla mb-12 text-center text-6xl font-extrabold text-amber-900 uppercase italic">
-        Meet The Board
-      </p>
-      <div className="mx-auto flex max-w-7xl flex-wrap justify-center gap-x-8 gap-y-16 px-4">
-        {boardMembers.map((member) => (
-          <BoardCard
-            key={member.email}
-            imagePath={member.imagePath}
-            name={member.name}
-            role={member.role}
-            major={member.major}
-            email={member.email}
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default BoardEntry;
-
-/* import Example from "@/components/example";
+import Example from "@/components/example";
 
 const Home = () => {
   return (
@@ -35,4 +8,4 @@ const Home = () => {
   );
 };
 
-export default Home; */
+export default Home;
