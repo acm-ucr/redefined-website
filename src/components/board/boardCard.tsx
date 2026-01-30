73ed0969319f476ea8df6cb6c743c7d91e31db70
EveningSt3r <daawesomeboi@gmail.com>
2026-01-30 01:48:55 -0800

    Font and color changes

--- src/components/board/boardCard.tsx
+++ src/components/board/boardCard.tsx
@@ -12,10 +12,10 @@ const BoardCard = ({ imagePath, name, role, major, email }: BoardCardProps) => {
   return (
     <div className="flex flex-shrink-0 p-4">
       <div className="relative h-135 w-80">
-        <div className="absolute inset-0 translate-x-4 translate-y-4 items-center rounded-3xl bg-amber-800"></div>
+        <div className="absolute inset-0 translate-x-4 translate-y-4 items-center rounded-3xl bg-redefined-rust"></div>
 
-        <div className="relative flex h-full flex-col items-center rounded-3xl bg-amber-50">
-          <div className="relative mt-15 mb-6 h-64 w-52 overflow-hidden rounded-3xl border-4 border-amber-800 shadow-sm">
+        <div className="relative flex h-full flex-col items-center rounded-3xl bg-redefined-cream">
+          <div className="relative mt-15 mb-6 h-64 w-52 overflow-hidden rounded-3xl border-4 bg-redefined-rust shadow-sm">
             <Image
               src={imagePath}
               alt={name}
@@ -26,16 +26,16 @@ const BoardCard = ({ imagePath, name, role, major, email }: BoardCardProps) => {
           </div>
 
           <div className="flex flex-col items-center gap-1">
-            <div className="font-serif text-4xl font-bold tracking-wide text-amber-800">
+            <div className="font-serif text-4xl font-bold tracking-wide text-redefined-rust">
               {name}
             </div>
-            <p className="text-xl font-black tracking-wider text-gray-800 uppercase">
+            <p className="text-xl font-redefined-alfa tracking-wider text-redefined-olive uppercase">
               {role}
             </p>
-            <div className="mt-2 space-y-1 text-lg font-black text-gray-600">
+            <div className="mt-2 space-y-1 text-lg font-semibold font-redefined-chivo text-redefined-olive">
               <p>{major}</p>
             </div>
-            <p className="mt-2 font-semibold text-black italic">{email}</p>
+            <p className="mt-2 font-semibold text-redefined-olive italic">{email}</p>
           </div>
         </div>
       </div>
