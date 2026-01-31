9de7b57c6dc29f7f174631824152442854b032b7
varunm532 <varundeep.pillai@gmail.com>
2026-01-31 11:44:45 -0800

    lander/joinbutton comments/fixes

---
+++ src/components/home/joinUs.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+import Picture from "@/public/join_us.png";
+const JoinUs = () => {
+  //things to do when button is clicked--needs to be changed depending on functionality--placeholder functionality currecntly
+  const handleClick = () => {
+    console.log("Join us clicked");
+    window.location.href = "/join";
+  };
+  //giving picture from figma button functionality
+  return (
+    <div className="bg-color-redefined-cream flex justify-center">
+      {/*giving button functionality to join button */}
+      <button onClick={handleClick}>
+        <Image src={Picture} alt="Join us Button" />
+      </button>
+    </div>
+  );
+};
+
+export default JoinUs;
