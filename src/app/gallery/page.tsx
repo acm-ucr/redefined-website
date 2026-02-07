7b81507acac72a7df5be17f9746e7f349b415a57
davastor <danhastor@gmail.com>
2026-02-06 21:38:40 -0800

    Used map function to generate images, created data file

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,10 +1,11 @@
 import GalleryEntry from "@/components/gallery/galleryEntry";
+import { galleryImages } from "@/data/galleryEntryInfo"
 
 const GalleryPage = () => {
   return (
     <div>
       Gallery
-      <GalleryEntry />
+      <GalleryEntry images={galleryImages}/>
     </div>
   );
 };
--- src/components/gallery/galleryEntry.tsx
+++ src/components/gallery/galleryEntry.tsx
@@ -1,26 +1,27 @@
-import Image from "next/image";
-import tablingPic from "@/public/tabling1Picture.svg";
-import heartMonitor from "@/public/galleryMeetingsLogo.svg";
+import Image, { StaticImageData } from "next/image";
+import heartMonitor from "@/public/gallery/galleryMeetingsLogo.svg";
 
-const GalleryEntry = () => {
+interface GalleryImageProp{
+    src: StaticImageData;
+    alt: string;
+}
+
+interface GalleryEntryProp {
+    images: GalleryImageProp[];
+}
+
+const GalleryEntry = ({images}: GalleryEntryProp) => {
   return (
     <div className="relative m-25 flex w-full justify-center">
       <div className="bg-redefined-rust relative z-10 flex w-10/12 justify-evenly gap-14 rounded-4xl p-14 drop-shadow-[15px_10px_0px_#5d5947]">
-        <Image
-          alt="Picture of club members with mascot"
-          src={tablingPic}
-          className="h-auto w-3/12 flex-1"
-        ></Image>
-        <Image
-          alt="Picture of club members with mascot"
-          src={tablingPic}
-          className="h-auto w-3/12 flex-1"
-        ></Image>
-        <Image
-          alt="Picture of club members with mascot"
-          src={tablingPic}
-          className="h-auto w-3/12 flex-1"
-        ></Image>
+        
+        {images.map((image) => (
+            <Image
+                alt={image.alt}
+                src={image.src}
+                className="h-auto w-3/12 object-cover flex-1"></Image>
+        ))}
+        
       </div>
       <p className="bg-redefined-cream font-redefined-chivo text-redefined-olive absolute -top-18 left-35 z-0 w-fit rounded-4xl px-7 pt-2 pb-8 text-6xl font-semibold tracking-widest italic">
         General Meetings
@@ -28,7 +29,7 @@ const GalleryEntry = () => {
       <Image
         alt="Picture of heart monitor icon"
         src={heartMonitor}
-        className="absolute -top-25 right-37 z-20"
+        className="absolute -top-27 right-37 z-20"
       ></Image>
     </div>
   );
