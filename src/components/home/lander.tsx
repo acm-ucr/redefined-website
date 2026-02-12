df55aab21c3d99a2019ad96906e15c633418be19
varunm532 <varundeep.pillai@gmail.com>
2026-02-12 15:59:59 -0800

    additions to lander component

--- src/components/home/lander.tsx
+++ src/components/home/lander.tsx
@@ -4,21 +4,21 @@ import Picture_Text from "@/public/Landing_pic_text.svg";
 const Lander = () => {
   return (
     //adds landing picture and makes it so that the picture's width is the entire screen
-    <div>
-      <div className="flex w-screen justify-center">
-        {/* split svg text and landing banner picture */}
-        <Image
-          className="absolute mt-[200px] flex w-1/3 items-center justify-center"
-          src={Picture_Text}
-          alt="Landing Picture"
-        />
-        {/* Landing banner image */}
-        <Image
-          className="flex w-full justify-center"
-          src={Picture}
-          alt="Landing Picture"
-        />
-      </div>
+
+    <div className="grid grid-cols-1 grid-rows-1">
+      {/* split svg text and landing banner picture */}
+
+      {/* Landing banner image */}
+      <Image
+        className="z-0 col-start-1 row-start-1 grid w-full"
+        src={Picture}
+        alt="Landing Picture"
+      />
+      <Image
+        className="z-10 col-span-20 col-start-1 row-start-1 self-center justify-self-center"
+        src={Picture_Text}
+        alt="Landing Picture"
+      />
     </div>
   );
 };
