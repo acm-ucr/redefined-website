a094307a652e07756fc8e2c5c19a57bb6be54905
varunm532 <varundeep.pillai@gmail.com>
2026-02-21 16:10:04 -0800

    changes to line style/placement

--- src/components/gallery/galleryHeader.tsx
+++ src/components/gallery/galleryHeader.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
 import Text from "@/public/GALLERY.svg";
-import Line from "@/public/Vector 3.png";
+import Line from "@/public/Vector 3.svg";
 
 const GalleryHeader = () => {
   return (
     <div className="ml-30 grid w-full grid-cols-[auto_1fr] items-center">
       {/* Left: Text */}
-      <Image src={Text} alt="Gallery" className="" />
+      <Image src={Text} alt="Gallery" />
 
       {/* Right: Line */}
 
-      <Image src={Line} alt="line design" className="h-2/3 w-full pl-50" />
+      <Image
+        src={Line}
+        alt="line design"
+        className="mb-30 -ml-20 h-6/10 w-screen"
+      />
     </div>
   );
 };
