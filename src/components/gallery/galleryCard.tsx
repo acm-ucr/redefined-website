19fdf3d86e82064f8a3207430b4952a7a4264f1d
davastor <danhastor@gmail.com>
2026-02-23 22:49:56 -0800

    Modified components to be mobile responsive

--- src/components/gallery/galleryCard.tsx
+++ src/components/gallery/galleryCard.tsx
@@ -25,40 +25,44 @@ const GalleryCard = ({
   const style = rightOrient
     ? {
         bgColor: "bg-redefined-olive",
-        dropShadow: "drop-shadow-[15px_10px_0px_#9e4325]",
-        textPos: "-top-18 right-16",
-        iconPos: "-top-24 left-25",
+        dropShadow:
+          "sm:drop-shadow-[15px_10px_0px_#9e4325] drop-shadow-[-8px_5px_0px_#9e4325]",
+        textPos: "sm:-top-18 sm:right-16 -top-7",
+        iconPos:
+          "sm:-top-24 sm:left-25 sm:right-auto sm:bottom-auto right-9 -bottom-4",
       }
     : {
         bgColor: "bg-redefined-rust",
-        dropShadow: "drop-shadow-[15px_10px_0px_#5d5947]",
-        textPos: "-top-18 left-17",
-        iconPos: "-top-26 right-15",
+        dropShadow:
+          "sm:drop-shadow-[15px_10px_0px_#5d5947] drop-shadow-[-8px_5px_0px_#5d5947]",
+        textPos: "sm:-top-18 sm:left-17 -top-7",
+        iconPos: "sm:-top-26 sm:right-15 sm:bottom-auto right-9 -bottom-4",
       };
 
   return (
     <div className="relative flex w-full justify-center">
+      <p
+        className={`bg-redefined-cream font-redefined-chivo text-redefined-olive absolute ${style.textPos} w-8/13 rounded-2xl pt-1 pb-8 text-center text-base font-semibold tracking-tighter italic sm:w-6/12 sm:rounded-4xl sm:pt-2 sm:text-6xl sm:tracking-widest`}
+      >
+        {title}
+      </p>
       <div
-        className={`relative z-10 flex w-14/15 justify-evenly gap-13 rounded-4xl p-14 ${style.bgColor} ${style.dropShadow}`}
+        className={`flex w-9/12 flex-col justify-evenly gap-6 rounded-4xl px-7 pt-9 py-14 leading-4 sm:w-14/15 sm:flex-row sm:gap-13 sm:p-14 ${style.bgColor} ${style.dropShadow}`}
       >
         {images.map((image) => (
           <Image
             key={image.id}
             alt={image.alt}
             src={image.src}
-            className={`bg-redefined-cream aspect-[5/4] w-3/12 flex-1 rounded-4xl object-cover p-2`}
+            className={`bg-redefined-cream aspect-[5/4] flex-1 rounded-2xl object-cover p-1 sm:w-3/12 sm:rounded-4xl sm:p-2`}
           ></Image>
         ))}
       </div>
-      <p
-        className={`bg-redefined-cream font-redefined-chivo text-redefined-olive absolute ${style.textPos} z-0 w-6/12 flex-none rounded-4xl pt-2 pb-8 text-center text-6xl font-semibold tracking-widest italic`}
-      >
-        {title}
-      </p>
+
       <Image
         alt={iconAlt}
         src={iconSrc}
-        className={`absolute ${style.iconPos} z-20`}
+        className={`absolute h-1/10 w-auto sm:h-auto ${style.iconPos}`}
       ></Image>
     </div>
   );
--- src/components/gallery/galleryEntry.tsx
+++ src/components/gallery/galleryEntry.tsx
@@ -3,8 +3,8 @@ import { galleryImages } from "@/data/galleryEntryInfo";
 
 const GalleryEntry = () => {
   return (
-    <div className="bg-redefined-cream w-11/12 rounded-4xl p-8">
-      <div className="bg-redefined-taupe flex flex-col gap-33 rounded-4xl pt-33 pb-18">
+    <div className="bg-redefined-cream rounded-4xl p-6 sm:w-11/12 sm:p-8">
+      <div className="bg-redefined-taupe flex flex-col gap-24 rounded-4xl pt-15 pb-9 sm:gap-33 sm:pt-33 sm:pb-18">
         {galleryImages.map(
           ({ title, iconSrc, iconAlt, id, rightOrient, images }) => {
             return (
