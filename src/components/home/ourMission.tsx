41a38e02d853f4ef96172c91b11f055d71d22fed
Kaesha Inocencio <freeforallyeet123@gmail.com>
2026-02-26 21:21:07 -0800

    modified responsifying landing page

--- src/components/home/ourMission.tsx
+++ src/components/home/ourMission.tsx
@@ -5,12 +5,12 @@ import JoinUs from "@/components/home/joinUs";
 const OurMissionComponent = () => {
   return (
     <div className="bg-redefined-taupe flex w-full flex-col items-center gap-12 pt-10">
-      <p className="bg-redefined-cream text-redefined-rust font-redefined-chivo w-11/12 rounded-4xl text-center text-6xl leading-12 font-medium tracking-widest italic drop-shadow-[0px_17px_0px_#5d5947] sm:h-20 sm:p-4 sm:text-5xl md:p-8">
+      <p className="bg-redefined-cream text-redefined-rust font-redefined-chivo w-11/12 rounded-4xl text-center text-base leading-12 font-medium tracking-widest italic drop-shadow-[0px_17px_0px_#5d5947] sm:text-3xl md:text-4xl lg:p-4 lg:text-6xl">
         OUR MISSION
       </p>
-      <div className="bg-redefined-cream flex w-3/4 items-start gap-2 rounded-3xl py-5 pr-13 pl-10 drop-shadow-[-20px_20px_0px_#5d5947] sm:h-130">
+      <div className="bg-redefined-cream flex w-3/4 items-start gap-2 rounded-3xl py-5 pr-13 pl-10 drop-shadow-[-20px_20px_0px_#5d5947]">
         <Image alt="Picture of heart" src={heartLogo}></Image>
-        <p className="font-redefined-albert text-center align-middle text-2xl leading-9 sm:text-lg">
+        <p className="font-redefined-albert text-center align-middle text-base leading-9 sm:text-lg md:text-xl lg:text-2xl">
           Our club is dedicated to educating students about street medicine and
           the needs of underserved communities. By working closely with our
           parent organization, we provide members not only with knowledge but
@@ -20,7 +20,7 @@ const OurMissionComponent = () => {
           create a lasting impact.
         </p>
       </div>
-      <div className="p-10 sm:w-100 sm:text-lg md:w-300 md:text-2xl">
+      <div className="p-10 text-base sm:w-100 sm:text-lg md:w-300 md:text-2xl">
         <JoinUs />
       </div>
     </div>
