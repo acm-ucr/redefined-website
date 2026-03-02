22a3f1f52a3b63cb25081be3c6545fcae2cce6a7
varunm532 <varundeep.pillai@gmail.com>
2026-03-01 22:29:17 -0800

    ran format

--- src/components/gallery/galleryHeader.tsx
+++ src/components/gallery/galleryHeader.tsx
@@ -5,13 +5,15 @@ const GalleryHeader = () => {
   return (
     <div className="-mb-10 ml-30 flex w-full grid-cols-[auto_1fr] items-center">
       {/* Left: Text */}
-      <div className=" mt-15 font-redefined-zilla font-[600] text-[150px] italic text-[#5D5947] tracking-[5%]">GALLERY</div>
+      <div className="font-redefined-zilla mt-15 text-[150px] font-[600] tracking-[5%] text-[#5D5947] italic">
+        GALLERY
+      </div>
       {/* Right: Line */}
 
       <Image
         src={Line}
         alt="line design"
-        className="mb-20 mt-10  h-7/10 w-screen"
+        className="mt-10 mb-20 h-7/10 w-screen"
       />
     </div>
   );
