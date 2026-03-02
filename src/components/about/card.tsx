a4e27d3369ee4b2e830164f2613a50c12e142a53
xxiyun <wesleylwu@gmail.com>
2026-03-01 21:59:54 -0800

    add background and padding

--- src/components/about/card.tsx
+++ src/components/about/card.tsx
@@ -4,33 +4,35 @@ import Ambulance from "@/public/Ambulance.webp";
 
 const About = () => {
   return (
-    <div className="bg-redefined-rust relative m-25 flex h-[58vh] w-[77vw] flex-col rounded-4xl p-8 shadow-[15px_10px_0px_#5d5947]">
-      <p className="text-redefined-cream font-redefined-chivo relative flex -translate-y-15 justify-end text-6xl font-bold italic">
-        GET TO KNOW US
-      </p>
-      <div className="grid grid-cols-2">
-        <p className="text-redefined-cream w-fit shrink pr-2 text-center text-xl">
-          redefineD at UCR is a club that stems from the redefineD nonprofit
-          organization. At our club, we aim to help students learn about the
-          topic of street medicine and providing healthcare services to
-          underserved populations. Our club started in the Fall Quarter of 2025.
-          redefineD at UCR values service, compassion, and empowerment. Through
-          our club, you will gain valuable experience, obtain new knowledge, and
-          be able to build a strong network of peers.
+    <div className="bg-redefined-taupe p-25">
+      <div className="bg-redefined-rust relative flex h-[58vh] w-[77vw] flex-col rounded-4xl p-8 shadow-[15px_10px_0px_#5d5947]">
+        <p className="text-redefined-cream font-redefined-chivo relative flex -translate-y-15 justify-end text-7xl font-bold italic">
+          GET TO KNOW US
         </p>
-        <div className="relative h-80 w-fit rounded-4xl">
-          <Image
-            src={Photo}
-            alt="Photo"
-            className="relative z-2 object-contain drop-shadow-[15px_10px_0px_#5d5947]"
-          ></Image>
+        <div className="grid grid-cols-2">
+          <p className="text-redefined-cream mt-20 w-11/12 shrink pr-2 text-center text-2xl">
+            redefineD at UCR is a club that stems from the redefineD nonprofit
+            organization. At our club, we aim to help students learn about the
+            topic of street medicine and providing healthcare services to
+            underserved populations. Our club started in the Fall Quarter of
+            2025. redefineD at UCR values service, compassion, and empowerment.
+            Through our club, you will gain valuable experience, obtain new
+            knowledge, and be able to build a strong network of peers.
+          </p>
+          <div className="relative h-80 w-fit rounded-4xl">
+            <Image
+              src={Photo}
+              alt="Photo"
+              className="relative z-2 object-contain drop-shadow-[15px_10px_0px_#5d5947]"
+            ></Image>
+          </div>
         </div>
+        <Image
+          src={Ambulance}
+          alt="Ambulance"
+          className="relative h-40 w-100 -translate-x-16 translate-y-36 object-contain"
+        ></Image>
       </div>
-      <Image
-        src={Ambulance}
-        alt="Ambulance"
-        className="relative h-40 w-100 -translate-x-8 -translate-y-8 object-contain"
-      ></Image>
     </div>
   );
 };
