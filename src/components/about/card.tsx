ca5e64cebdbc33ca8591d051ce5104edac6a2afc
Kevin Pember <Kevinkzp@gmail.com>
2026-03-04 21:15:26 -0800

    Removing Extra tailwind classes

--- src/components/about/card.tsx
+++ src/components/about/card.tsx
@@ -5,12 +5,12 @@ import Ambulance from "@/public/Ambulance.svg";
 const About = () => {
   return (
     <div className="bg-redefined-taupe flex justify-center p-20">
-      <div className="bg-redefined-rust relative flex h-fit w-[95vw] flex-col rounded-4xl p-8 shadow-[15px_10px_0px_#5d5947]">
-        <p className="text-redefined-cream font-redefined-chivo relative -top-10 -m-8 flex justify-end text-7xl font-bold italic">
+      <div className="bg-redefined-rust relative flex w-[95vw] flex-col rounded-4xl p-8 shadow-[15px_10px_0px_#5d5947]">
+        <p className="text-redefined-cream font-redefined-chivo relative -top-10 -m-8 text-right text-7xl font-bold italic">
           GET TO KNOW US
         </p>
         <div className="grid grid-cols-2">
-          <p className="text-redefined-cream shrink pr-2 text-center text-xl">
+          <p className="text-redefined-cream pr-2 text-center text-xl">
             redefineD at UCR is a club that stems from the redefineD nonprofit
             organization. At our club, we aim to help students learn about the
             topic of street medicine and providing healthcare services to
