f4ac60a316e7cb40d83090d1b1bcca63cdc8bdc2
davastor <danhastor@gmail.com>
2026-03-10 14:42:17 -0700

    Added animations for landing component

--- src/components/home/lander.tsx
+++ src/components/home/lander.tsx
@@ -1,19 +1,29 @@
+"use client";
 import Image from "next/image";
 import Picture from "@/public/Landing_Pic.webp";
 import Picture_Text from "@/public/Landing_pic_text.svg";
+import { motion } from "motion/react";
+
 const Lander = () => {
   return (
     <div className="grid grid-cols-1 grid-rows-1 place-items-center">
-      <Image
+      <motion.div
+        initial={{ opacity: 0.25 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.2 }}
         className="z-0 col-start-1 row-start-1 grid w-full"
-        src={Picture}
-        alt="Landing Picture"
-      />
-      <Image
+      >
+        <Image src={Picture} alt="Landing Picture" />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 15 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
         className="z-10 col-start-1 row-start-1"
-        src={Picture_Text}
-        alt="Landing Picture"
-      />
+      >
+        <Image src={Picture_Text} alt="Landing Picture" />
+      </motion.div>
     </div>
   );
 };
