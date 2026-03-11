989957ee92d5b0371542ab4b035f3a73eff1c167
davastor <danhastor@gmail.com>
2026-03-10 23:41:34 -0700

    Formatted using prettier and checked with eslint

--- src/components/home/lander.tsx
+++ src/components/home/lander.tsx
@@ -5,20 +5,22 @@ import Picture_Text from "@/public/Landing_pic_text.svg";
 import { motion } from "motion/react";
 
 const pictAnimation = {
-    initial:{ opacity: 0.25 },
-    whileInView:{opacity: 1, transition:{ duration: 0.2 }}
+  initial: { opacity: 0.25 },
+  whileInView: { opacity: 1, transition: { duration: 0.2 } },
 };
 
 const textAnimation = {
-    initial:{ opacity: 0, y: 15 },
-    whileInView: {opacity: 1, y: 0, transition:{ duration: 0.5 }}
+  initial: { opacity: 0, y: 15 },
+  whileInView: { opacity: 1, y: 0, transition: { duration: 0.5 } },
 };
 
 const Lander = () => {
   return (
     <div className="grid grid-cols-1 grid-rows-1 place-items-center">
       <motion.div
-        variants={pictAnimation} initial="initial" whileInView="whileInView"
+        variants={pictAnimation}
+        initial="initial"
+        whileInView="whileInView"
         className="z-0 col-start-1 row-start-1 grid w-full"
       >
         <Image
@@ -29,7 +31,9 @@ const Lander = () => {
       </motion.div>
 
       <motion.div
-        variants={textAnimation} initial="initial" whileInView="whileInView"
+        variants={textAnimation}
+        initial="initial"
+        whileInView="whileInView"
         className="z-10 col-start-1 row-start-1"
       >
         <Image src={Picture_Text} alt="Landing Picture" />
