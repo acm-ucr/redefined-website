92b347e03451c6bf7a217b4da0f70697a1107cda
davastor <danhastor@gmail.com>
2026-03-10 23:39:01 -0700

    Declared animations outside of div

--- src/components/home/lander.tsx
+++ src/components/home/lander.tsx
@@ -4,22 +4,28 @@ import Picture from "@/public/Landing_Pic.webp";
 import Picture_Text from "@/public/Landing_pic_text.svg";
 import { motion } from "motion/react";
 
+const pictAnimation = {
+    initial:{ opacity: 0.25 },
+    whileInView:{opacity: 1, transition:{ duration: 0.2 }}
+};
+
+const textAnimation = {
+    initial:{ opacity: 0, y: 15 },
+    whileInView: {opacity: 1, y: 0, transition:{ duration: 0.5 }}
+};
+
 const Lander = () => {
   return (
     <div className="grid grid-cols-1 grid-rows-1 place-items-center">
       <motion.div
-        initial={{ opacity: 0.25 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.2 }}
+        variants={pictAnimation} initial="initial" whileInView="whileInView"
         className="z-0 col-start-1 row-start-1 grid w-full"
       >
         <Image src={Picture} alt="Landing Picture" />
       </motion.div>
 
       <motion.div
-        initial={{ opacity: 0, y: 15 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
+        variants={textAnimation} initial="initial" whileInView="whileInView"
         className="z-10 col-start-1 row-start-1"
       >
         <Image src={Picture_Text} alt="Landing Picture" />
