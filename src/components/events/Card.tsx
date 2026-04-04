2e4ca7246a25e52aff94072c59052916e05be16e
Kevin Pember <73316239+Kevin-Pember@users.noreply.github.com>
2026-04-03 18:37:01 -0700

    Bit more changes

--- src/components/events/Card.tsx
+++ src/components/events/Card.tsx
@@ -1,27 +1,31 @@
 "use client";
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
+import monitor from "@/public/calendar/monitor.svg";
+import bottle from "@/public/calendar/bottle.svg";
+import board from "@/public/calendar/board.svg";
 
 interface CardProps {
   title: string;
   location: string;
   date: string;
   time: string;
-  image: StaticImageData;
   index: number;
 }
 
-const Card = ({ title, location, date, time, image, index }: CardProps) => {
+const icons = [board, bottle, monitor];
+const Card = ({ title, location, date, time, index }: CardProps) => {
   const mode = index % 2 == 0;
-  const lineStyling = `h-1 w-full border-0 ${mode ? " bg-redefined-rust" : " bg-redefined-cream"}`;
+  const lineStyling = `h-1 w-full rounded-full border-0 ${mode ? " bg-redefined-rust" : " bg-redefined-cream"}`;
+  const icon = icons[index % 3];
   return (
-    <div className="flex w-[33vw] flex-col">
+    <div className="relative flex w-[33vw] flex-col place-items-center">
       <p
-        className={`text-center text-xl font-bold md:text-2xl ${mode ? "text-redefined-cream bg-redefined-rust" : "text-redefined-rust bg-redefined-cream"}`}
+        className={`w-4/5 text-center text-xl font-bold md:text-2xl ${mode ? "text-redefined-cream bg-redefined-rust" : "text-redefined-rust bg-redefined-cream"}`}
       >
         {title}
       </p>
       <div
-        className={`${mode ? "text-redefined-rust bg-redefined-cream" : "text-redefined-cream bg-redefined-rust"} flex flex-col items-center justify-around rounded-t-md p-3 text-center`}
+        className={`w-full rounded-4xl py-12 ${mode ? "text-redefined-rust bg-redefined-cream" : "text-redefined-cream bg-redefined-rust"} flex flex-col items-center justify-around rounded-t-md p-3 text-center`}
       >
         <hr className={lineStyling} />
         <p className="text-2xl font-bold md:text-3xl">{date}</p>
@@ -29,7 +33,7 @@ const Card = ({ title, location, date, time, image, index }: CardProps) => {
         <p className="text-base font-bold md:text-lg">{location}</p>
         <hr className={lineStyling} />
       </div>
-      <Image src={image} alt="Event Icon" />
+      <Image className="absolute right-0 bottom-0" src={icon} alt="Icon" />
     </div>
   );
 };
